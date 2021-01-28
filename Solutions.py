a1ef537dbb57bc3aeff03f8da85d6631977b7b69
Alex Lu <77261922+Alex-4018@users.noreply.github.com>
2021-01-27 22:40:38 -0800

    Create Solutions.py

---
+++ Solutions.py
@@ -0,0 +1,69 @@
+#1. Two Sum
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        h = {}
+        for i, num in enumerate(nums):
+            n = target - num
+            if n not in h:
+                h[num] = i
+            else:
+                return [h[n], i]
+
+
+#7. Reverse Integer
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
